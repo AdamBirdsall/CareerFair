3b099479ddf8fc44683002d7d45cab4ce20724c8
Adam Birdsall <adam.birdsall@ticketmaster.com>
2016-03-02 14:23:34 -0700

    Initial Commit

---
+++ Career Fair/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Career Fair
+//
+//  Created by Adam Birdsall on 3/2/16.
+//  Copyright © 2016 Adam Birdsall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
