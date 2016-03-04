92aae6254fc2a2dd0badcb15a7b980956044f407
Adam Birdsall <adam.birdsall@ticketmaster.com>
2016-03-04 14:56:53 -0700

    updated code. doesn't work exactly

--- Career Fair/ViewController.swift
+++ Career Fair/ViewController.swift
@@ -25,8 +25,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    // Google form URL
+    // https://docs.google.com/forms/d/1OD_2Zs-TzZv3E_ZfFjnSn1I6CIXO-Ma8b3UUm2JDKjc/formResponse
+    
+    // Name
+    // entry.2005620554
+    
+    // Email
+    // entry.1045781291
+    
+    // Address
+    // entry.1065046570
+    
     @IBAction func clickSubmit(sender: AnyObject) {
         
+        
+        // Google sheets way
+        //*********************
+        let url = NSURL(string: "https://docs.google.com/forms/d/1OD_2Zs-TzZv3E_ZfFjnSn1I6CIXO-Ma8b3UUm2JDKjc/formResponse")
+        var postData = "entry.2005620554" + "=" + firstName.text!
+        postData += "&" + "entry.1045781291" + "=" + lastName.text!
+        postData += "&" + "entry.1065046570" + "=" + emailField.text!
+        postData += "&submit=Submit"
+        
+        let request = NSMutableURLRequest(URL: url!)
+        request.HTTPMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = postData.dataUsingEncoding(NSUTF8StringEncoding)
+//        var connection = NSURLConnection(request: request, delegate: nil, startImmediately: true)
+        
+        
+        // Parse way
+        //*********************
         let newStudent = PFObject(className: "SignUps")
         newStudent["First"] = firstName.text
         newStudent["Last"] = lastName.text
@@ -49,17 +80,6 @@ class ViewController: UIViewController {
             }
         }
     }
-    
-    // Google form URL
-    // https://docs.google.com/forms/d/1OD_2Zs-TzZv3E_ZfFjnSn1I6CIXO-Ma8b3UUm2JDKjc/formResponse
 
-    // Name
-    // entry.2005620554
-    
-    // Email
-    // entry.1045781291
-    
-    // Address
-    // entry.1065046570
 }
 
